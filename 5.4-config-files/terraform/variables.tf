09d61ff0cdbf5a2be4b768243096de38e433a480
Vitaliy N <xoma4ok.83@mail.ru>
2023-02-21 09:09:52 +0300

    5.3 5.4 add config files

---
+++ 5.4-config-files/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g8roqvnljdcrg08i45"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1ge21a1um8g6t7jqndj"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8jvcoeij6u9se84dt5"
+}

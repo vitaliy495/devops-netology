f7354a3e963c68dcf4e62a3125b9abd73a2d9230
Vitaliy N <nekrasov.vitaliy@gmail.com>
2023-05-15 22:33:43 +0300

    7.3 (4)

--- 7.3-terraform/src/ansible.tf
+++ 7.3-terraform/src/ansible.tf
@@ -1,38 +1,37 @@
 resource "local_file" "hosts_cfg" {
-  sleep= 30 
-  #depends_on = [yandex_compute_instance.vm-each, yandex_compute_instance.web, yandex_compute_instance.wm-disk]
+  depends_on = [ yandex_compute_instance.web, yandex_compute_instance.wm-disk]
   content = templatefile("${path.module}/hosts.tftpl",
 
-    { web = yandex_compute_instance.web, 
+    { web = yandex_compute_instance.web,
       wm-each = yandex_compute_instance.vm-each,
-      wm-disk = yandex_compute_instance.wm-disk
-    }
+      wm-disk = [yandex_compute_instance.wm-disk]
+     }
   )
 
   filename = "${abspath(path.module)}/hosts.cfg"
 }
 
-variable "public_key" {
-  type    = string
-  default = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-}
+
+resource "null_resource" "web_hosts_provision" {
+#Ждем создания инстанса
+depends_on = [yandex_compute_instance.vm-each]
 
 #Добавление ПРИВАТНОГО ssh ключа в ssh-agent
-  provisioner "local-exec" {
-    command = "cat ~/.ssh/id_ed25519 | ssh-add -"
-  }
+# provisioner "local-exec" {
+#    command = "eval $(ssh-agent -s) | cat ~/.ssh/id_ed25519 | ssh-add -"
+# }
 
 #Запуск ansible-playbook
-  provisioner "local-exec" {                  
+ provisioner "local-exec" {                  
     command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
     on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
     environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
     #срабатывание триггера при изменении переменных
-  }
+ }
     triggers = {
       always_run         = "${timestamp()}" #всегда т.к. дата и время постоянно изменяются
       playbook_src_hash  = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
-      ssh_public_key     = var.public_key # при изменении переменной
+      ssh_public_key     = "${file("~/.ssh/id_ed25519.pub")}" # при изменении переменной
     }
 
 }
